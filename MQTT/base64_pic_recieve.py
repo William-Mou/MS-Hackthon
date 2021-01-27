3056c3746adacc68cc6918eec997575b56100fe5
william <william.mou960174@gmail.com>
2021-01-27 22:22:47 +0800

    Add mqtt

---
+++ MQTT/base64_pic_recieve.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import base64
+import paho.mqtt.client as mqtt
+
+# This is the Subscriber
+
+def on_connect(client, userdata, flags, rc):
+  print("Connected with result code "+str(rc))
+  client.subscribe("topic/image")
+
+def _on_message(client, userdata, msg):
+  if msg.payload.decode() == "Hello world!":
+    print("Yes!")
+    client.disconnect()
+
+def on_message(client, obj, msg):
+ # with open('/home/yicheng/python/test/new', 'wb') as fd:
+    #fd.write(msg.payload)
+    imgdata = base64.b64decode(msg.payload)
+    filename = 'some_image.jpg'
+    with open(filename, 'wb') as f:
+       f.write(imgdata)
+    client.disconnect()
+
+
+client = mqtt.Client()
+client.connect("140.114.91.173",1883,60)
+client.on_connect = on_connect
+#///////////////////////////////////////////////////
+client.on_message = on_message
+client.loop_forever()
---
+++ MQTT/base64_pic_send.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import paho.mqtt.client as mqtt
+import base64
+
+with open("test.jpg", "rb") as img_file:
+    my_string = base64.b64encode(img_file.read())
+
+
+# This is the Publisher
+client = mqtt.Client()
+client.connect("140.114.91.173",1883,60)
+#client.publish("topic/test", "Hello world!");
+#f=open("/home/yicheng/python/test/test.txt")
+byteArray = bytes(my_string)
+client.publish("topic/test", byteArray ,0)
+print(byteArray)
+client.disconnect();
+
+
+
+
